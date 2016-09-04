516c88c0cc3066470af81bda65490c22a67a3621
AxelGoetz <axel.goetz@hotmail.com>
2016-09-04 10:49:45 +0200

    Add memory to escape HTML

---
+++ public/js/utils/stuff.js
@@ -0,0 +1,11 @@
+function escapeHTML( text ) {
+    if (text && text.length > 0) {
+      return text.replace( /&/g, "&amp;" )
+                 .replace( /</g, "&lt;" )
+                 .replace( />/g, "&gt;" )
+                 .replace( /"/g, "&quot;" )
+                 .replace( /'/g, "&#39;" );
+    } else {
+      return "";
+    }
+  }
